949b765446494d7a0f030b9a7b59868830aeaa28
2alheure <jojo77.juv@live.fr>
2021-03-22 15:45:32 +0100

    Image upload for Article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -66,6 +70,23 @@ class ArticleController extends AbstractController {
             ]);
         } else {
 
+            // Je vais déplacer le fichier uploadé
+
+            // On récupère l'image
+            $image = $form->get('image')->getData();
+            // On définit le nom du fichier
+            $fileName =  uniqid() . '.' . $image->guessExtension();
+
+            try {
+                // On déplace le fichier
+                $image->move($this->getParameter('article_image_directory'), $fileName);
+            } catch (FileException $ex) {
+                $form->addError(new FormError('Une erreur est survenue pendant l\'upload du fichier : ' . $ex->getMessage()));
+                throw new Exception('File upload error');
+            }
+
+            $article->setImage($fileName);
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($article);
             $em->flush();
@@ -82,6 +103,8 @@ class ArticleController extends AbstractController {
         $repo = $this->getDoctrine()->getRepository(Article::class);
         $article = $repo->find($id);
 
+        $oldImage = $article->getImage();
+
         if (empty($article)) throw new NotFoundHttpException();
 
         $form = $this->createForm(ArticleType::class, $article);
@@ -91,9 +114,23 @@ class ArticleController extends AbstractController {
         if (!$form->isSubmitted() || !$form->isValid()) {
             return $this->render('article/modifier-article.html.twig', [
                 'form' => $form->createView(),
+                'oldImage' => $oldImage,
                 'id' => $article->getId()
             ]);
         } else {
+
+            // Je vais déplacer le fichier uploadé
+            $image = $form->get('image')->getData();
+
+            try {
+                $image->move($this->getParameter('article_image_directory'), $oldImage);
+            } catch (FileException $ex) {
+                $form->addError(new FormError('Une erreur est survenue pendant l\'upload du fichier : ' . $ex->getMessage()));
+                throw new Exception('File upload error');
+            }
+
+            $article->setImage($oldImage);
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($article);
             $em->flush();
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -5,12 +5,13 @@ namespace App\Form;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ArticleType extends AbstractType
 {
@@ -20,7 +21,20 @@ class ArticleType extends AbstractType
             ->add('titre', TextType::class)
             ->add('date', DateType::class)
             ->add('contenu', TextareaType::class)
-            ->add('image', UrlType::class)
+            ->add('image', FileType::class, [
+                'label' => 'Image d\'illustration de l\'article',
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ]
+                    ]),
+                ]
+            ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Envoyer'
             ])
