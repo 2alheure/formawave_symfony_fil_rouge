add9df05b2d2dede04d61eb4a1f19fc4ac1ea0c4
2alheure <jojo77.juv@live.fr>
2021-03-24 12:24:04 +0100

    Example of role use

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,6 +15,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 class ArticleController extends AbstractController {
     /**
@@ -48,6 +50,8 @@ class ArticleController extends AbstractController {
 
     /**
      * @Route("/gerer-articles", name="gerer_articles")
+     * 
+     * @IsGranted("ROLE_ADMIN")
      */
     public function gererArticles(): Response {
         $repository = $this->getDoctrine()->getRepository(Article::class);
